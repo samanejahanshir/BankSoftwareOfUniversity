2f69f09b821e8bbaa71d08a956ee4b587f4eca9c
unknown <jahanshir.sm92@gmail.com>
2021-11-27 23:44:55 -0800

    add equals and hashSet in Disc class

--- src/main/java/ir/maktab/models/Disc.java
+++ src/main/java/ir/maktab/models/Disc.java
@@ -1,4 +1,34 @@
 package ir.maktab.models;
 
+import java.util.Objects;
+
 public class Disc {
+    private  String name;
+    private  TypeDisc type;
+
+    public Disc(String name, TypeDisc type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public TypeDisc getType() {
+        return type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Disc disc = (Disc) o;
+        return name.equals(disc.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
