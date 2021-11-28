6979e062aba995bf312db8a42cd117f314951410
unknown <jahanshir.sm92@gmail.com>
2021-11-27 22:51:24 -0800

    add property and getter and setter in Borrow class

--- src/main/java/ir/maktab/models/Borrow.java
+++ src/main/java/ir/maktab/models/Borrow.java
@@ -1,4 +1,24 @@
 package ir.maktab.models;
 
 public class Borrow {
+    private  String discName;
+    private  Date date;
+
+    public Borrow(String disc, Date date) {
+        this.discName = disc;
+        this.date = date;
+    }
+
+    public String getDiscName() {
+        return discName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+    public  boolean isLate(Date deliveryDate){
+        //////TODO
+        return false;
+    }
+
 }
