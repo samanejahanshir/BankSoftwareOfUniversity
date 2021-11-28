3e87f056fec97830b32935910a5c1ee4d4b20b89
unknown <jahanshir.sm92@gmail.com>
2021-11-28 10:10:58 -0800

    add class DateDifferenceDatesTest in test directory

---
+++ src/test/java/ir/maktab/models/DateDifferenceDatesTest.java
@@ -0,0 +1,22 @@
+package ir.maktab.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.text.ParseException;
+
+public class DateDifferenceDatesTest {
+   @Test
+    void givenNumber_WhereDifferenceDatesCalls_ThenResponseReturn() throws ParseException {
+        Date date1=new Date(98,7,7);
+        Date date2=new Date(98,7,15);
+        long result = Date.differenceDates(date1,date2);
+        Assertions.assertEquals(8, result);
+       Date date3=new Date(98,7,10);
+       Date date4=new Date(98,7,23);
+       long result2 = Date.differenceDates(date3,date4);
+       Assertions.assertEquals(13, result2);
+    }
+}
