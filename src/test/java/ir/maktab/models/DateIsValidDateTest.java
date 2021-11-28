baff5fa629d3aabb04154547ebbef8be5de205e9
unknown <jahanshir.sm92@gmail.com>
2021-11-28 09:58:25 -0800

    add class DateIsValidDateTest in test directory

---
+++ src/test/java/ir/maktab/models/DateIsValidDateTest.java
@@ -0,0 +1,29 @@
+package ir.maktab.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class DateIsValidDateTest {
+Date date;
+    @BeforeEach
+    void init() {
+        System.out.println("before each test ");
+        date = new Date(1,2,34);
+    }
+    @ParameterizedTest
+    @CsvSource({"89,10,2", "98,3,14"})
+    void givenNumber_WhereIsValidDateCalls_ThenResponseReturn(int year, int month, int day) {
+        boolean result = Date.isValidDate(year,month,day);
+        Assertions.assertEquals(true, result);
+    }
+    @ParameterizedTest
+    @CsvSource({"89,13,2", "98,3,33"})
+    void givenNumber_WhereIsValidDateCalls_ThenResponseReturnException(int year, int month, int day) {
+        RuntimeException exp=Assertions.assertThrows(RuntimeException.class,()->
+                Date.isValidDate(year,month,day));
+        System.out.println(exp.getMessage());
+        Assertions.assertEquals("format date is invalid",exp.getMessage());
+    }
+}
