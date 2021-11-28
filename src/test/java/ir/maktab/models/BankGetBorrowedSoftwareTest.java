491138532b5e669e70c23b9df50e4f503df756b0
unknown <jahanshir.sm92@gmail.com>
2021-11-28 11:40:36 -0800

    add BankGetBorrowedSoftwareTest class in test directory

--- src/test/java/ir/maktab/models/BankGetBorrowedSoftwareTest.java
+++ src/test/java/ir/maktab/models/BankGetBorrowedSoftwareTest.java
@@ -3,7 +3,7 @@ package ir.maktab.models;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class BankGetPersonAmountFineTest {
+public class BankGetBorrowedSoftwareTest {
     Bank bank;
 
     @BeforeEach
@@ -11,10 +11,9 @@ public class BankGetPersonAmountFineTest {
         System.out.println("before each test ");
         bank = new Bank();
     }
-
     @Test
     void givenNumber_WhereGetLateDateCalls_ThenResponseReturn() {
-        for (String s : bank.getPersonAmountFine()) {
+        for (String s : bank.getBorrowedSoftware()) {
             System.out.println(s);
         }
     }
