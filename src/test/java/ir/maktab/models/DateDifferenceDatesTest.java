63884ca6edf2f3e3783ad4738534ac1ff3e45bae
unknown <jahanshir.sm92@gmail.com>
2021-11-28 10:23:57 -0800

    add class PersonGetLateDateTest in test directory

--- src/test/java/ir/maktab/models/DateDifferenceDatesTest.java
+++ src/test/java/ir/maktab/models/DateDifferenceDatesTest.java
@@ -2,8 +2,6 @@ package ir.maktab.models;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import java.text.ParseException;
 
---
+++ src/test/java/ir/maktab/models/PersonGetLateDateTest.java
@@ -0,0 +1,27 @@
+package ir.maktab.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.util.List;
+
+public class PersonGetLateDateTest {
+    Person person;
+    @BeforeEach
+    void init() {
+        System.out.println("before each test ");
+        person=new Person("sara");
+    }
+    @Test
+    void givenNumber_WhereGetLateDateCalls_ThenResponseReturn() throws ParseException {
+       person.borrow(new Disc("java"),new Date(99,7,6));
+        person.borrow(new Disc("java"),new Date(99,7,17));
+        long day=person.getLateDate("java",0);
+        Assertions.assertEquals(11, day);
+
+
+
+    }
+}
