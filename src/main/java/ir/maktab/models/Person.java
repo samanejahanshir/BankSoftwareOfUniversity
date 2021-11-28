ca63a17fea7e2e54473c7b53a82fd6d1a31dcdc3
unknown <jahanshir.sm92@gmail.com>
2021-11-27 22:57:19 -0800

    add property and getter and setter in Person class

--- src/main/java/ir/maktab/models/Person.java
+++ src/main/java/ir/maktab/models/Person.java
@@ -1,4 +1,17 @@
 package ir.maktab.models;
 
 public class Person {
+    private  String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public  int getLateDate(){
+        ////////////TODO
+        return  0;
+    }
 }
