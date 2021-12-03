984452c841c4e9add5659650aa71993fae970d38
unknown <jahanshir.sm92@gmail.com>
2021-12-02 23:48:28 -0800

    fix block static in class Bank

--- src/main/java/ir/maktab/models/Bank.java
+++ src/main/java/ir/maktab/models/Bank.java
@@ -5,17 +5,23 @@ import java.util.Set;
 
 public class Bank {
     public static Set<Person> personSet = new HashSet<>();
-    public static Set<Disc> discSet;
+    public static Set<DiscImp> discSet;
     public static int fine = 200;
     public static final String PATH_FILE_INFO = "resource/info.csv";
     public static final String TITr_INPUT = "day month year personName softwareName\n";
 
     static {
-        discSet = Set.of(new Disc("java")
-                , new Disc("office")
-                , new Disc("photoshop")
-                , new Disc("powerpoint")
-                , new Disc("whats app")
-                , new Disc("maya"));
+       /* discSet = Set.of(new DiscImp("java")
+                , new DiscImp("office")
+                , new DiscImp("photoshop")
+                , new DiscImp("powerpoint")
+                , new DiscImp("whats app")
+                , new DiscImp("maya"));*/
+        discSet = Set.of(DiscFactory.getDiscByType(TypeDisc.CD,"java")
+                ,DiscFactory.getDiscByType(TypeDisc.CD,"office")
+                , DiscFactory.getDiscByType(TypeDisc.DVD,"photoshop")
+                , DiscFactory.getDiscByType(TypeDisc.DVD,"powerpoint")
+                , DiscFactory.getDiscByType(TypeDisc.CD,"whats app")
+                ,DiscFactory.getDiscByType(TypeDisc.DVD,"maya"));
     }
 }
